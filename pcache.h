67f81926fa3a5dada7a8a686d761a9ddcaac90c4
Bin Liu <bliu@carpetshark.ics.cs.cmu.edu>
2012-12-02 00:42:22 -0500

    cache v1

--- pcache.h
+++ pcache.h
@@ -13,5 +13,27 @@
 #include "string.h"
 #include "csapp.h"
 
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+typedef struct {
+	int is_valid;
+	int timestamp;
+    char *uri_key ;		/* use uri as key of the cache */
+    char *webobj_buf;   /* pointer to the cached obj */
+} linePCache;
+
+static linePCache *centralCache;
+
+void init_cache();
+
+/* Exposed interfaces */
+int is_cached(char *uri_in);
+char* get_webobj_from(char *uri_in);
+char* get_buf_for_webobj(char *uri_in);
+
+/* Internal helpers*/
+ 
+
 #endif /* __PCACHE_H__ */
 /* $end pcache.h */
