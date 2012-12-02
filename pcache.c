d4686fdb2f6d28397a53cd54e9e2513bc0503cb5
Bin Liu <binliutech@gmail.com>
2012-12-01 21:52:06 -0500

    added pcache files

---
+++ pcache.c
@@ -0,0 +1,12 @@
+/*
+ * pcache.c
+ *
+ * Author: Bin Liu (bliu) & Lei Yan (leiyan)
+ * implementation file for cache module
+ */
+
+/* $begin pcache.c */
+#include "pcache.h"
+
+ 
+/* $end pcache.c */
---
+++ pcache.h
@@ -0,0 +1,17 @@
+/*
+ * pcache.h
+ *
+ * Author: Bin Liu (bliu) & Lei Yan (leiyan)
+ * header file for cache module
+ */
+
+/* $begin pcache.h */
+#ifndef __PCACHE_H__
+#define __PCACHE_H__
+
+#include <stdio.h>
+#include "string.h"
+#include "csapp.h"
+
+#endif /* __PCACHE_H__ */
+/* $end pcache.h */
